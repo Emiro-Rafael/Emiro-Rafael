cc6532070fe8423884cf0bffb4ba88b3233a280d
Emiro-Rafael <joanalsagarcia1128@gmail.com>
2024-11-06 16:47:12 -0600

    Merge pull request #406 from SnackCrate/barcode_lookup

--- wp-content/themes/wp-bootstrap-starter-child/header.php
+++ wp-content/themes/wp-bootstrap-starter-child/header.php
@@ -14,7 +14,7 @@ global $is_redesign_page;
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <meta name="description" content="Your one-stop shop for delicious snacks and candy from around the world">
